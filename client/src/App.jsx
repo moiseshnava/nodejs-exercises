0acf808e30e64fd07c381f2710802810d881acc9
moiseshnava <moises92nava@gmail.com>
2023-05-11 21:51:18 -0600

    finis exercise 4

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -20,13 +20,13 @@ function App() {
       body: message,
       from: "Me"
     }
-    setMessages([newMessage,...messages]);
+    setMessages([newMessage, ...messages]);
     setMessage(initialState.message);
   }
 
   useEffect(() => {
     const receivedMessage = (message) => {
-      setMessages([...messages, message])
+      setMessages([message, ...messages])
     }
 
     socket.on("message", receivedMessage);
@@ -39,18 +39,33 @@ function App() {
 
   return (
     <>
-      <div>
-        <form onSubmit={e => handleSubmit(e)}>
-          <input type="text" name="" id="" onChange={e => setMessage(e.target.value)} value={message} />
-          <button>send</button>
-        </form>
-        {
-          messages && messages.map((message) => (
-            <div >
-              <p>{`${message.from} : ${message.body}`}</p>
-            </div>
-          ))
-        }
+      <div className='flex flex-col h-screen items-center justify-center bg-zinc-800 text-white'>
+        <div className='flex flex-col bg-zinc-950 p-2 justify-center items-center '>
+          <h1 className='font-bold text-2xl text-center'>Chat React</h1>
+          <form
+            className='flex flex-col rounded-lg bg-zinc-950 p-10 mb-4'
+            onSubmit={e => handleSubmit(e)}>
+            <input
+              className='w-full border-2 border-zinc-500 p-2 rounded-lg text-black'
+              type="text" name="" id=""
+              onChange={e => setMessage(e.target.value)}
+              value={message}
+              placeholder='Write a message...'
+            />
+            <button className='h-10 justify-center items-center bg-blue-500 rounded-xl mt-2 '>send</button>
+          </form>
+          <ul className='h-80 overflow-auto p-2'>
+            {
+              messages && messages.map((message, index) => (
+                <li key={index}
+                  className={`my-1 table p-2 rounded-md ${message.from === "Me" ? "bg-sky-700 ml-auto" : "bg-zinc-700"}`}
+                >
+                  <p>{`${message.from} : ${message.body}`}</p>
+                </li>
+              ))
+            }
+          </ul>
+        </div>
       </div>
 
     </>
