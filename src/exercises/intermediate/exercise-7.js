b86d708a4e59f41356f072fd7c1a2bbf33c023a8
moiseshnava <moises92nava@gmail.com>
2023-05-12 22:24:31 -0600

    Finish exercise 7

--- src/exercises/intermediate/exercise-7.js
+++ src/exercises/intermediate/exercise-7.js
@@ -1,1 +1,30 @@
 // Crea una aplicación Node.js que utilice el módulo Cron para programar tareas periódicas.
+
+// # ┌────────────── second (optional)
+// # │ ┌──────────── minute
+// # │ │ ┌────────── hour
+// # │ │ │ ┌──────── day of month
+// # │ │ │ │ ┌────── month
+// # │ │ │ │ │ ┌──── day of week
+// # │ │ │ │ │ │
+// # │ │ │ │ │ │
+// # * * * * * *
+
+const CronJob = require("cron").CronJob;
+
+const job = new CronJob("*/5 * * * * *", ()=> {
+   console.log("Esta tarea se ejecutara cada 5s");
+});
+
+const job2 = new CronJob("*/1 * * * *",()=>{
+   console.log("Esta tarea se ejecutara cada 1 minuto");
+});
+
+const job3 = new CronJob("*/1 * * * * *", ()=> {
+   console.log("Esta tarea se ejecutara cada 1s");
+});
+
+
+job.start();
+job2.start();
+job3.start();
